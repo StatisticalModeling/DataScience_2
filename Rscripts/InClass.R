cb1d73310d867a8ddda678399c66585f66d927c7
Alan T. Arnholt <arnholtat@appstate.edu>
2024-02-13 12:55:29 -0500

    tweakers

--- Rscripts/InClass.R
+++ Rscripts/InClass.R
@@ -1,13 +1,38 @@
 # Read in data
 Credit <- read.csv("http://statlearning.com/s/Credit.csv")
-mod_null <- lm(Rating ~ 1, data = Credit)
-mod_full <- lm(Rating ~ ., data = Credit)
+mod_null <- lm(Balance ~ 1, data = Credit)
+mod_full <- lm(Balance ~ ., data = Credit)
 library(MASS)
 stepAIC(mod_null, scope = list(lower = mod_null, upper = mod_full), direction = "forward")
 ####
-mod_aic <- lm(Rating ~ Limit + Cards + Married + Student + Education, data = Credit)
+mod_aic <- lm(Balance ~ Rating + Income + Student + Limit + Cards + Age, data = Credit)
 summary(mod_aic)
 
 mod_for <- mod_null
-?add1
+SCOPE <- (~ Income + Limit + Rating + Cards + Age + Education + Own + Student + Married + Region)
 
+add1(mod_for, scope = SCOPE, test = "F")
+mod_for <- update(mod_for, .~. + Rating)
+add1(mod_for, scope = SCOPE, test = "F")
+mod_for <- update(mod_for, .~. + Income)
+add1(mod_for, scope = SCOPE, test = "F")
+mod_for <- update(mod_for, .~. + Student)
+add1(mod_for, scope = SCOPE, test = "F")
+mod_for <- update(mod_for, .~. + Limit)
+add1(mod_for, scope = SCOPE, test = "F")
+mod_for <- update(mod_for, .~. + Cards)
+add1(mod_for, scope = SCOPE, test = "F")
+mod_for <- update(mod_for, .~. + Age)
+add1(mod_for, scope = SCOPE, test = "F")
+
+####################
+mod_be <- lm(Balance ~ ., data = Credit)
+drop1(mod_be, scope = SCOPE, test = "F")
+mod_be <- update(mod_be, .~. - Education)
+drop1(mod_be, test = "F")
+mod_be <- update(mod_be, .~. - Region)
+drop1(mod_be, test = "F")
+mod_be <- update(mod_be, .~. - Married)
+drop1(mod_be, test = "F")
+mod_be <- update(mod_be, .~. - Own)
+drop1(mod_be, test = "F")
