c564d33aa49a898b2c509635550a340aa08717e1
Alan T. Arnholt <arnholtat@appstate.edu>
2024-02-15 09:16:34 -0500

    tweakers

--- Rscripts/InClass.R
+++ Rscripts/InClass.R
@@ -1,11 +1,23 @@
 # Read in data
 Credit <- read.csv("http://statlearning.com/s/Credit.csv")
+names(Credit)
+library(tidyverse)
+Credit %>% 
+  summarize(MB = mean(Balance),
+          n = n(),
+          SD = sd(Balance))
+ggplot(data = Credit, aes(x = Balance)) + 
+  geom_histogram(fill = "blue", color = "purple") +
+  theme_bw()
+
 mod_null <- lm(Balance ~ 1, data = Credit)
 mod_full <- lm(Balance ~ ., data = Credit)
 library(MASS)
-stepAIC(mod_null, scope = list(lower = mod_null, upper = mod_full), direction = "forward")
+stepAIC(mod_null, scope = list(lower = mod_null, upper = mod_full), 
+        direction = "forward")
 ####
-mod_aic <- lm(Balance ~ Rating + Income + Student + Limit + Cards + Age, data = Credit)
+mod_aic <- lm(Balance ~ Rating + Income + Student + Limit + 
+                Cards + Age, data = Credit)
 summary(mod_aic)
 
 mod_for <- mod_null
@@ -36,3 +48,56 @@ mod_be <- update(mod_be, .~. - Married)
 drop1(mod_be, test = "F")
 mod_be <- update(mod_be, .~. - Own)
 drop1(mod_be, test = "F")
+
+###################
+## Answering the questions
+
+mod_null <- lm(Rating ~ 1, data = Credit)
+mod_full <- lm(Rating ~ ., data = Credit)
+mod_for <- stepAIC(mod_null, scope = list(lower = mod_null, upper = mod_full), direction = "forward")
+summary(mod_for)
+mod_bac <- stepAIC(mod_full, scope = list(lower = mod_null, upper = mod_full), direction = "backward")
+summary(mod_bac)
+####
+mod_five <- lm(Rating ~ Limit + Cards + Married + Student + Education, data = Credit)
+summary(mod_five)
+####
+# Credit Rating
+predict(mod_five, newdata = data.frame(Limit = c(6000, 12000), 
+                                       Cards = c(4, 4), 
+                                       Married = c("Yes", "Yes"),
+                                       Student = c("No", "No"), 
+                                       Education = c(16, 8)),
+        interval = "conf", level = .99)
+
+predict(mod_five, newdata = data.frame(Limit = c(6000, 12000), 
+                                       Cards = c(4, 4), 
+                                       Married = c("Yes", "Yes"),
+                                       Student = c("No", "No"), 
+                                       Education = c(16, 8)),
+        interval = "pred", level = .99)
+
+#### Linear Algebra
+
+X <- model.matrix(mod_five)
+X
+XTX <- t(X)%*%X
+XTX
+XTXI <- solve(XTX)
+zapsmall(XTXI %*% XTX)
+y <- Credit$Rating
+betahat <- XTXI%*%t(X)%*%y
+betahat
+summary(mod_five)$coef
+### recall that the better way to get XTXI is with cov.unscaled
+summary(mod_five)$cov.unscaled -> XTXI
+XTXI
+MSE <- summary(mod_five)$sigma^2
+MSE
+####
+anova(mod_five)
+anova(mod_five)[6,3] -> MSE
+####
+varcov <- MSE*XTXI
+varcov
+
